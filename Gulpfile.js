473943ce4810f61b4e149b8a26273132ed7a172d
strarsis <strarsis@gmail.com>
2015-11-23 22:22:25 +0100

    Initial commit.

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var Eyeglass = require("eyeglass").Eyeglass;
+var sassOptions = {
+  includePaths: ['/mnt/share/application/shared/scss/']
+}; // put whatever eyeglass and node-sass options you need here.
+
+var eyeglass = new Eyeglass(sassOptions);
+
+eyeglass.enableImportOnce = true;
+
+gulp.task("sass", function () {
+  gulp.src("./sass/**/*.scss")
+    .pipe(sass(eyeglass.sassOptions()).on("error", sass.logError))
+    .pipe(gulp.dest("./css"));
+});
